74d3e59086613532a720a93b811988fcd1cccfc8
Alexey Medvedev <a.medvedev@itfbgroup.ru>
2022-04-02 13:13:57 +0300

    feat: добавлен экспорт настроек

--- app/src/main/java/alex/studio/csvsearcher/enums/Properties.java
+++ app/src/main/java/alex/studio/csvsearcher/enums/Properties.java
@@ -1,27 +1,30 @@
 package alex.studio.csvsearcher.enums;
 
+import android.content.res.Resources;
 import android.os.Environment;
 
 import alex.studio.csvsearcher.R;
 
 public enum Properties {
 
-    CSV_LINK("https://www.pais.co.il/chance/chance_resultsDownload.aspx", 0),
-    CSV_FOLDER(Environment.getExternalStorageDirectory() + "/Chance", 0),
-    PDF_FOLDER(null, 0),
-    FIRST_LAUNCH(null, 0),
-    ALGO_1(null, R.string.algo_name_1_set_val),
-    ALGO_2(null, R.string.algo_name_2_set_val),
-    ALGO_3(null, R.string.algo_name_3_set_val),
-    ALGO_4(null, R.string.algo_name_4_set_val),
-    ALGO_5(null, R.string.algo_name_5_set_val),
-    ALGO_6(null, R.string.algo_name_6_set_val);
+    CSV_LINK("csv_link", "https://www.pais.co.il/chance/chance_resultsDownload.aspx", 0),
+    CSV_FOLDER("csv_folder", Environment.getExternalStorageDirectory() + "/Chance", 0),
+    PDF_FOLDER("pdf_folder", null, 0),
+    FIRST_LAUNCH("first_launch", null, 0),
+    ALGO_1("algorithm_1_name", null, R.string.algo_name_1_set_val),
+    ALGO_2("algorithm_2_name", null, R.string.algo_name_2_set_val),
+    ALGO_3("algorithm_3_name", null, R.string.algo_name_3_set_val),
+    ALGO_4("algorithm_4_name", null, R.string.algo_name_4_set_val),
+    ALGO_5("algorithm_5_name", null, R.string.algo_name_5_set_val),
+    ALGO_6("algorithm_6_name", null, R.string.algo_name_6_set_val);
 
-    Properties(String defaultVal, int resId) {
+    Properties(String key, String defaultVal, int resId) {
         this.defaultValue = defaultVal;
         this.resId = resId;
+        this.key = key;
     }
 
+    private final String key;
     private final String defaultValue;
     private final int resId;
 
@@ -31,5 +34,18 @@ public enum Properties {
 
     public int getResId() {
         return this.resId;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    public static Properties fromKey(String key) {
+        for (Properties prop : Properties.values()) {
+            if (prop.key.equals(key)) {
+                return prop;
+            }
+        }
+        throw new Resources.NotFoundException();
     }
 }
--- app/src/main/java/alex/studio/csvsearcher/ui/SettingActivity.java
+++ app/src/main/java/alex/studio/csvsearcher/ui/SettingActivity.java
@@ -1,18 +1,5 @@
 package alex.studio.csvsearcher.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import alex.studio.csvsearcher.R;
-import alex.studio.csvsearcher.components.StorageManager;
-import alex.studio.csvsearcher.enums.Properties;
-
 import static alex.studio.csvsearcher.enums.Properties.ALGO_1;
 import static alex.studio.csvsearcher.enums.Properties.ALGO_2;
 import static alex.studio.csvsearcher.enums.Properties.ALGO_3;
@@ -23,15 +10,38 @@ import static alex.studio.csvsearcher.enums.Properties.CSV_FOLDER;
 import static alex.studio.csvsearcher.enums.Properties.CSV_LINK;
 import static alex.studio.csvsearcher.utils.FileUtil.getFullPathFromTreeUri;
 import static alex.studio.csvsearcher.utils.ViewUtils.getTextFrom;
-import static alex.studio.csvsearcher.utils.ViewUtils.isEmpty;
 import static alex.studio.csvsearcher.utils.ViewUtils.isNotEmpty;
 
-import java.util.Optional;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+
+import alex.studio.csvsearcher.R;
+import alex.studio.csvsearcher.components.StorageManager;
+import alex.studio.csvsearcher.enums.Properties;
 
 public class SettingActivity extends AppCompatActivity {
 
-    private final int READ_REQUEST_CODE_CSV = 1;
-    private final int READ_REQUEST_CODE_PDF = 2;
+    private static final int READ_REQUEST_CODE_CSV = 1;
+    private static final int READ_REQUEST_CODE_PDF = 2;
+    private static final int READ_REQUEST_CODE_IMPORT = 3;
+    private static final int READ_REQUEST_CODE_EXPORT = 4;
+    private static final String IMPORT_FILE_NAME = "application.properties";
 
     private EditText editCsvLink;
     private EditText editFirstAlgoName;
@@ -44,15 +54,18 @@ public class SettingActivity extends AppCompatActivity {
     private View btnChangeCsvFolder;
     private View btnSave;
     private View btnCancel;
+    private View btnImport;
+    private View btnExport;
 
     private TextView textCsvFolder;
-    //private TextView textPdfFolder;
+    private StorageManager storageManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
+        storageManager = new StorageManager(SettingActivity.this);
         initView();
     }
 
@@ -60,20 +73,37 @@ public class SettingActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == READ_REQUEST_CODE_CSV) {
-            try {
-                textCsvFolder.setText(getFullPathFromTreeUri(data.getData(),
-                        SettingActivity.this));
-            } catch (Exception ex) {
-                textCsvFolder.setText("");
-            }
-        } /*else if (requestCode == READ_REQUEST_CODE_PDF) {
-            try {
-                textPdfFolder.setText(data.getData().toString());
-            } catch (Exception ex) {
-                textPdfFolder.setText("");
-            }
-        }*/
+        switch (requestCode) {
+            case READ_REQUEST_CODE_CSV:
+                try {
+                    textCsvFolder.setText(getFullPathFromTreeUri(data.getData(),
+                            SettingActivity.this));
+                } catch (Exception ex) {
+                    textCsvFolder.setText("");
+                }
+                break;
+            case READ_REQUEST_CODE_EXPORT:
+                try {
+                    exportToFile(getFullPathFromTreeUri(data.getData(),
+                            SettingActivity.this));
+                    Toast.makeText(SettingActivity.this,
+                            "Настройки успешно экспортированы", Toast.LENGTH_SHORT).show();
+                } catch (Exception ex) {
+                    Toast.makeText(SettingActivity.this,
+                            "Невозможно экспортировать настройки", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case READ_REQUEST_CODE_IMPORT:
+                try {
+                    importFromFile(data.getData());
+                    Toast.makeText(SettingActivity.this,
+                            "Настройки успешно импортрованы", Toast.LENGTH_SHORT).show();
+                } catch (Exception ex) {
+                    Toast.makeText(SettingActivity.this,
+                            "Невозможно импортировать настройки", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 
     private boolean isDataValid() {
@@ -83,6 +113,8 @@ public class SettingActivity extends AppCompatActivity {
     private void initView() {
         editCsvLink = findViewById(R.id.editCsvLink);
 
+        btnImport = findViewById(R.id.btnImport);
+        btnExport = findViewById(R.id.btnExport);
         btnChangeCsvFolder = findViewById(R.id.btnChangeCsvFolder);
         //btnChangePdfFolder = findViewById(R.id.btnChangePdfFolder);
 
@@ -106,15 +138,19 @@ public class SettingActivity extends AppCompatActivity {
 
         //btnChangePdfFolder.setOnClickListener(v -> performFileSearch(READ_REQUEST_CODE_PDF));
 
-        btnChangeCsvFolder.setOnClickListener(v -> performFileSearch(READ_REQUEST_CODE_CSV));
+        btnChangeCsvFolder.setOnClickListener(v -> performFileSearch(READ_REQUEST_CODE_CSV, true));
 
         btnCancel.setOnClickListener(v -> closeActivity());
 
         btnSave.setOnClickListener(v -> saveAction());
+
+        btnImport.setOnClickListener(v -> performFileSearch(READ_REQUEST_CODE_IMPORT, false));
+
+        btnExport.setOnClickListener(v -> performFileSearch(READ_REQUEST_CODE_EXPORT, true));
     }
 
     private void saveAction() {
-        if(isDataValid()) {
+        if (isDataValid()) {
             StorageManager storageManager = new StorageManager(SettingActivity.this);
             storageManager.write(Properties.CSV_FOLDER, getTextFrom(textCsvFolder));
             //storageManager.write(Properties.PDF_FOLDER, getTextFrom(textPdfFolder));
@@ -134,7 +170,6 @@ public class SettingActivity extends AppCompatActivity {
     }
 
     private void initValue() {
-        StorageManager storageManager = new StorageManager(SettingActivity.this);
         editCsvLink.setText(storageManager.read(CSV_LINK));
         textCsvFolder.setText(storageManager.read(CSV_FOLDER));
         editFirstAlgoName.setText(storageManager.read(ALGO_1));
@@ -146,10 +181,99 @@ public class SettingActivity extends AppCompatActivity {
         //textPdfFolder.setText(storageManager.read(PDF_FOLDER));
     }
 
-    private void performFileSearch(final int requestCode) {
-        Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+    private void exportToFile(String path) throws IOException {
+        File file = new File(path + "/" + IMPORT_FILE_NAME);
+        StringBuilder sb = new StringBuilder();
+        sb.append(createSettingsString(CSV_LINK, editCsvLink));
+        sb.append(createSettingsString(CSV_FOLDER, textCsvFolder));
+        sb.append(createSettingsString(ALGO_1, editFirstAlgoName));
+        sb.append(createSettingsString(ALGO_2, editTwoAlgoName));
+        sb.append(createSettingsString(ALGO_3, editThreeAlgoName));
+        sb.append(createSettingsString(ALGO_4, editFourAlgoName));
+        sb.append(createSettingsString(ALGO_5, editFiveAlgoName));
+        sb.append(createSettingsString(ALGO_6, editSixAlgoName));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write(sb.toString());
+        writer.close();
+    }
+
+    private void importFromFile(Uri uri) throws IOException {
+        if (!uri.toString().endsWith(".properties")) {
+            Toast.makeText(SettingActivity.this, "Неверный формат файла",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        InputStream is = getContentResolver().openInputStream(uri);
+        BufferedInputStream reader = new BufferedInputStream(is);
+        byte[] info = new byte[reader.available()];
+        reader.read(info, 0, info.length);
+        String data = new String(info);
+        String[] lines = data.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            if (!lines[i].isEmpty()) {
+                String[] values = lines[i].split(":", 2);
+                if (values.length == 2) {
+                    try {
+                        Properties prop = Properties.fromKey(values[0]);
+                        storageManager.write(prop, values[1]);
+                        getTextViewByProp(prop).setText(values[1]);
+                    } catch (Resources.NotFoundException ex) {
+                        Log.d("IMPORT", "Resource not found exception");
+                    }
+                }
+            }
+        }
+        is.close();
+        reader.close();
+    }
+
+    private TextView getTextViewByProp(Properties prop) {
+        TextView view;
+        switch (prop) {
+            case ALGO_1:
+                view = editFirstAlgoName;
+                break;
+            case ALGO_2:
+                view = editTwoAlgoName;
+                break;
+            case ALGO_3:
+                view = editThreeAlgoName;
+                break;
+            case ALGO_4:
+                view = editFourAlgoName;
+                break;
+            case ALGO_5:
+                view = editFiveAlgoName;
+                break;
+            case ALGO_6:
+                view = editSixAlgoName;
+                break;
+            case CSV_FOLDER:
+                view = textCsvFolder;
+                break;
+            case CSV_LINK:
+                view = editCsvLink;
+                break;
+            default:
+                throw new Resources.NotFoundException();
+        }
+        return view;
+    }
+
+    private String createSettingsString(Properties prop, View editText) {
+        return String.format("%s:%s\n", prop.getKey(), getTextFrom(editText));
+    }
+
+    private void performFileSearch(final int requestCode, boolean selectFolder) {
+        Intent i = new Intent(selectFolder ? Intent.ACTION_OPEN_DOCUMENT_TREE :
+                Intent.ACTION_GET_CONTENT);
         i.addCategory(Intent.CATEGORY_DEFAULT);
-        startActivityForResult(Intent.createChooser(i, "Choose directory"), requestCode);
+        if (!selectFolder) {
+            i.setType("*/*");
+            i.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        }
+        startActivityForResult(Intent.createChooser(i, selectFolder ? "Выберите папку" :
+                "Выберите файл"), requestCode);
     }
 
     private void closeActivity() {
